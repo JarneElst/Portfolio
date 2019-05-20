6be6209b56b02aeb3d36f9d048e675f0d4490b4d
Jarne.Elst <jarne.elst@student.scheppersinstituut.be>
2019-05-20 20:43:14 +0200

    Carousel komik

--- projecten.php
+++ projecten.php
@@ -59,6 +59,42 @@ include("./php/header.php");
             </div>
             <div class="projecten-box col-12 col-md-11">
                 <h1 class="titel-light">Komiktoneel</h1>
+
+                <div class="bd-example">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="./images/proj-komik-before.png" class="d-block w-100"
+                                    alt="Komiktoneel before">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h6>Before</h6>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./images/proj-komik-after.png" class="d-block w-100" alt="
+                                    Komiktoneel after">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h6>After</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+
                 <figure>
                     <img src="./images/Proj-Komiktoneel.png" alt="Foto Chenesse" class="projecten-picture" />
                 </figure>
